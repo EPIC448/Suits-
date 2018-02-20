9bd2647b373cdbf8e7e4e95f8fc0ce08a399e983
EPIC448 <samrey2018@gmail.com>
2018-02-20 19:01:25 +0000

    added deal file into lib/blacklapel

--- lib/blacklapel/cli.rb
+++ lib/blacklapel/cli.rb
@@ -1,4 +1,4 @@
- # This is  CLI controller
+# This is  CLI controller
 class Blacklapel::CLI
 
   def call
@@ -8,33 +8,30 @@ class Blacklapel::CLI
 
   end
 
-    def suit_deals
-      #here doc  https://blacklapel.com/
-      puts "Today's suits"
-      puts  "<<-Doc.gsub/^\s*/, ''
-      1. suit jacket -$390-
-      2. double breasted  $911"
+  def suit_deals
+    #here doc  https://blacklapel.com/
+    @deal = "make sure it works"
 
-    end
+  end
 
-    def choice
-      input = nil
-      while input != "exit"
-       input = gets.strip.downcase
-       case input
-       when "1"
-         puts "More info on deal 1..."
-       when "2"
-         puts "More info on deal 2..."
-       when "choice"
-         choice
-       else
-         puts "not sure what you want, type list or exit"
-       end
-     end
+  def choice
+    input = nil
+    while input != "exit"
+      input = gets.strip.downcase
+      case input
+      when "1"
+        puts "More info on deal 1..."
+      when "2"
+        puts "More info on deal 2..."
+      when "choice"
+        choice
+      else
+        puts "not sure what you want, type suit_list or exit"
+      end
     end
+  end
 
-    def goodbye
-      puts 'see you tomorrow'
-    end
+  def goodbye
+    puts 'see you tomorrow'
+  end
 end
---
+++ lib/blacklapel/deal.rb
@@ -0,0 +1,29 @@
+class Blacklapel::Deal
+  attr_accessor :name, :price, :avalibilty, :url
+    def self.today
+      #return all instances of a deal
+    puts "Today's suits"
+    puts  "<<-Doc.gsub/^\s*/, ''
+    2. suit jacket -$360-
+    1. double breasted  $549"
+
+   # blacklapel
+    deal_1 = self.new
+    deal-1.name = "double breasted"
+    price = "$549"
+    avalibilty = true
+    url = "https://blacklapel.com/"
+
+
+
+    # macys
+    deal_2 = self.new
+    deal-2.name = "suit jacket"
+    price = "$360"
+    avalibilty = true
+    Url = "https://www.macys.com/shop/b/suit-jacket?id=74300"
+
+#  must return [deal_1, deal_2]
+
+    end
+end
