5af86ca45cdef9e90886b5d53165beb56c46c1ff
EPIC448 <samrey2018@gmail.com>
2018-02-21 18:43:51 +0000

    setting up scraper in deals

--- lib/blacklapel/deal.rb
+++ lib/blacklapel/deal.rb
@@ -1,28 +1,33 @@
 class Blacklapel::Deal
   attr_accessor :name, :price, :avalibilty, :url
+  # we are combining the scrape method into our deals rather than doing them seperatly..
+ # scrape blacklap & macys.
 
     def self.today
-      #return all instances of a deal by scraping blacklapel and macy. then return the data.
-    # puts "Today's suits"
-    # puts  "<<-Doc.gsub/^\s*/, ''
-    # 2. suit jacket -$360-
-    # 1. double breasted  $549"
-
-   # blacklapel
-    deal_1 = self.new
-    deal-1.name = "double breasted"
-    price = "$549"
-    avalibilty = true
-    url = "https://blacklapel.com/"
-
-    # macys
-    deal_2 = self.new
-    deal-2.name = "suit jacket"
-    price = "$360"
-    avalibilty = true
-    url = "https://www.macys.com/shop/b/suit-jacket?id=74300"
-
-#  must return [deal_1, deal_2]
-
+        self.scrape_deals
     end
+
+ #blacklapel
+
+      def self.scrape_deals
+         # Got to blacklapel, Fing product
+         #Extract the properties
+         #instantiate a deal.
+        deal_1 = self.new
+        deal-1.name = "double breasted"
+        price = "$549"
+        avalibilty = true
+        url = "https://blacklapel.com/"
+
+        # macys
+        deal_2 = self.new
+        deal-2.name = "suit jacket"
+        price = "$360"
+        avalibilty = true
+        url = "https://www.macys.com/shop/b/suit-jacket?id=74300"
+
+        #  must return array of sracpped deals. [deal_1, deal_2]
+
+      end
+
 end
