dc104f4c8b9559ae923087b84274785b4173b146
EPIC448 <samrey2018@gmail.com>
2018-02-21 21:24:49 +0000

    Scrapping lab

--- lib/blacklapel.rb
+++ lib/blacklapel.rb
@@ -1,6 +1,7 @@
 require'open-uri' #it uri not you URL
 require 'nokogiri'
 require 'pry'
+require 'capybara/poltergeist'
 require_relative "./blacklapel/version"
 require_relative './blacklapel/deal'
 require_relative './blacklapel/cli'
