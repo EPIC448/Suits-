e1dc782f0d0c0d0d4c57d878122979e666c38289
EPIC448 <samrey2018@gmail.com>
2018-02-22 22:44:46 +0000

    updated deal create new functionality

--- lib/blacklapel/deal.rb
+++ lib/blacklapel/deal.rb
@@ -16,11 +16,14 @@ class Blacklapel::Deal
 
       def self.scrape_mens_warehouse
        doc = Nokogiri::HTML(open("https://www.menswearhouse.com/mens-clothes/mens-suit-separates/suit-separate-coats/pronto-uomo-platinum-modern-fit-suit-separates-coat-navy-sharkskin-343F344F81"))
-       name = doc.search("h1.prod-title").text
-       price = doc.search("review-price.final-price regular > p").text
-       url = doc.search("a.product-info-label")
-       avalibilty =true
 
+       deal.name = doc.search("h1.prod-title").text
+       deal.price = doc.search("review-price.final-price regular > p").text #recheck price
+       deal.url = doc.search("a.product-info-label").first.attr("href")
+       deal.avalibilty =true
+
+
+       deal = self.new
          binding.pry
       # deal_2 = self.new
       # deal-2.name = "suit jacket"
