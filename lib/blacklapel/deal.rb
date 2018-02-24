c14ab3e37a5bd28ee5dd095371b2f0447e4dc07d
EPIC448 <samrey2018@gmail.com>
2018-02-24 17:55:08 +0000

    scrapped mens wear house:

--- lib/blacklapel/deal.rb
+++ lib/blacklapel/deal.rb
@@ -2,43 +2,43 @@
 class Blacklapel::Deal
   attr_accessor :name, :price, :avalibity, :url
   # we are combining the scrape method into our scrape method using the mens wear house  site.
-    def self.today
-        self.scrape_deals
-    end
+  def self.today
+    self.scrape_deals
+  end
 
 
-      def self.scrape_deals  # for all deals.
-        #40 mins
-        deals = []
-        # so for instance the first page has 100 products, your app should be able to pull those 100 and then you should be able to drill down into any one of those
-         deals << self.scrape_mens_warehouse
-        deals << self.scrape_Homepage_mens_warehouse
+  def self.scrape_deals  # for all deals.
+    #40 mins
+    deals = []
+    # so for instance the first page has 100 products, your app should be able to pull those 100 and then you should be able to drill down into any one of those
+    deals << self.scrape_Homepage_mens_warehouse
+    deals << self.scrape_mens_warehouse
+    deals
+  end
 
-        deals
-      end
+  def self.scrape_Homepage_mens_warehouse
+    doc = Nokogiri::HTML(open("https://www.menswearhouse.com/mens-suits"))
+    deal = self.new
+    #HUGE TOOL FOR SCRAPING ****Selector Gadget**** Targets what is most needed.
+    deal.name = doc.css(".icon-tmw-logo-black , .current-page").text.strip
+    deal.price = doc.css("p#offerPrice_700395328.product-price-discount").text #recheck price
+    deal.url = "https://menswearhouse.com"
+    deal.avalibity = doc.css("p#promotionStr_700734302").text
 
-        def self.scrape_Homepage_mens_warehouse
-          doc = Nokogiri::HTML(open("https://www.menswearhouse.com/mens-suits"))
-          deal = self.new
-          binding.pry
-          #HUGE TOOL FOR SCRAPING ****Selector Gadget**** Targets what is most needed.
-          deal.name = doc.css("#searchBasedNavigation_widget").text.strip
-          deal.price = doc.css("p#offerPrice_700395328.product-price-discount").text #recheck price
-          deal.url = "https://menswearhouse.com"
-          deal.avalibity = doc.css("p#promotionStr_700734302").text
+    deal   # Always call the method.
 
-        end
+  end
   # deals mins 30 -38
-     def self.scrape_mens_warehouse
-       doc = Nokogiri::HTML(open("https://menswearhouse.com/mens-suits/slim-fit-extra-trim-suits/calvin-klein-navy-plaid-extreme-slim-fit-suit-37LX37LY61"))
-       deal = self.new
-       #HUGE TOOL FOR SCRAPING ****Selector Gadget**** Targets what is most needed.
-       deal.name = doc.css(".prod-title").text.strip
-       deal.price = doc.css(".regular").text #recheck price
-       deal.url = "https://menswearhouse.com"
-       deal.avalibity = doc.css("#findInStoreAccordion , .js-swatch-item:nth-child(5) img").text
+  def self.scrape_mens_warehouse
+    doc = Nokogiri::HTML(open("https://menswearhouse.com/mens-suits/slim-fit-extra-trim-suits/calvin-klein-navy-plaid-extreme-slim-fit-suit-37LX37LY61"))
+    deal = self.new
+    #HUGE TOOL FOR SCRAPING ****Selector Gadget**** Targets what is most needed.
+    deal.name = doc.css(".prod-title").text.strip
+    deal.price = doc.css(".regular").text
+    deal.url = "https://menswearhouse.com"
+    deal.avalibity = doc.css("#findInStoreAccordion , .js-swatch-item:nth-child(5) img").text
 
-       deal
-      end
+    deal
+  end
 
 end
