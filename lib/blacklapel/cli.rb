134615aa1daa94580539a800022dc66769b8ba2d
Sam. O <samrey2018@gmail.com>
2018-03-09 18:57:54 +0000

    updated the cli.rb

--- lib/blacklapel/cli.rb
+++ lib/blacklapel/cli.rb
@@ -3,14 +3,15 @@
 class Blacklapel::CLI
 
   def call
-    suit_deals
+    puts "Today's Daily Deals"
+    # suit_deals
     choice
     goodbye
 
   end
 
   def suit_deals  #
-    puts "Today's Daily Deals"
+
     @deal = Blacklapel::Deal.scrape_Homepage_mens_warehouse
     @deal.each.with_index(1) do |deal, i|
     # possible (iterate over deal again.. becasue is an array.. so it only return one thing in the array,  or set it up in "today's method to to return only on thing in the array"
@@ -22,6 +23,17 @@ class Blacklapel::CLI
   def choice
     input = nil
     while input != "exit"
+      puts "Please type:"
+      puts ""
+      puts "Type in the suit number you would like to see"
+      puts ""
+      puts "OR"
+      puts ""
+      puts "Type 'suit' to view all suit listings"
+      puts ""
+      puts "OR"
+      puts ""
+      puts "Type 'exit' to exit"
       input = gets.strip.downcase
       i = input.to_i-1
 
@@ -31,7 +43,7 @@ class Blacklapel::CLI
       elsif input == "suit"
         suit_deals
       else
-        puts "not sure what you want, type suit or exit"
+        puts "not sure what you want"
       end
     end
   end
