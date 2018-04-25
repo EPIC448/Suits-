0ae66a4a16f2c5e61c87607fec3c0f269f6da153
epic0448 <samuelogundiran@Yahoo.com>
2018-04-25 15:17:35 -0400

    works great.. Updated.

--- lib/blacklapel/deal.rb
+++ lib/blacklapel/deal.rb
@@ -1,19 +1,6 @@
 class Blacklapel::Deal
   attr_accessor :name, :price, :avalibity, :url
-  # we are combining the scrape method into our scrape method using the mens wear house  site.
 
-  # def self.today
-  #   self.scrape_deals
-  # end
-  #
-  #
-  # def self.scrape_deals  # for all deals.
-  #   #40 mins
-  #   deals = []
-  #   # so for instance the first page has 100 products, your app should be able to pull those 100 and then you should be able to drill down into any one of those
-  #   deals << self.scrape_Homepage_mens_warehouse
-  #   deals
-  # end
 
   def self.scrape_Homepage_mens_warehouse
     doc = Nokogiri::HTML(open("https://www.menswearhouse.com/mens-suits"))
